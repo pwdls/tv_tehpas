f42697cb27c54d452c7a7f469fa086442589236d
Gribov Igor <work.gribov@gmail.com>
2020-06-28 19:54:50 +0300

    START

---
+++ class/PWDLS_mysql.class.php
@@ -0,0 +1,71 @@
+<?php
+
+class PWDLS_mysql
+{
+    /*    private $host = '127.0.0.1';
+      private $db = 'sitemanager0';
+      private $user = 'magok';
+      private $pass = 'ehj;fq'; */
+
+    private $host;
+    private $db;
+    private $user;
+    private $pass;
+    private $mysql;
+
+    private function __construct($id = 0)
+    {
+        if ($id == 0) {
+            $this->useIt_product();
+        } else {
+            $this->useMag();
+        }
+        $this->mysql = new \mysqli($this->host, $this->user, $this->pass, $this->db);
+        if (!$this->mysql) {
+            echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+            echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+        } else {
+            $this->mysql->query('SET NAMES utf8');
+        }
+    }
+
+    private function useMag()
+    {
+        $this->host = '127.0.0.1';
+        $this->db = 'tehpas';
+        $this->user = 'root';
+        $this->pass = '';
+    }
+
+    private function useIt_product()
+    {
+        $this->host = '127.0.0.1';
+        $this->db = 'tehpas';
+        $this->user = 'root';
+        $this->pass = '';
+    }
+
+    static public function getMySQL($mas, $id = 0)
+    {
+        $foo = new PWDLS_mysql($id);
+        if (gettype($mas) == 'array') {
+            $i = 1;
+            while ($i <= count($mas)) {
+                $res = $foo->mysql->query($mas[$i - 1]);
+                $i++;
+            }
+        } else {
+            $res = $foo->mysql->query($mas);
+        }
+        $foo->mysql->close();
+        if ($res->num_rows != 0) {
+            while ($row = $res->fetch_array()) {
+                $result[] = $row;
+            }
+        } else {
+            $result = 'FALSE';
+        }
+        return $result;
+    }
+
+}
---
+++ update.php
@@ -0,0 +1,75 @@
+<?php
+define('ARHIV', filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . include_once ("conf/whereArhiv--dir-cfg.php"));
+include_once 'class/PWDLS_mysql.class.php';
+
+
+function main(){
+    $dir = include_once("conf/whereToGetFoles--file-cfg.php");
+    $arrDir = getArrDir($dir);
+
+    foreach ($arrDir as $value){
+        setData($dir , $value);
+    }
+}
+
+function foo($foo){
+    echo '<pre>';
+    var_dump($foo);
+    echo '</pre>';
+}
+
+function getArrDir($dir){
+    $result = scandir($dir);
+    unset($result[0]);
+    unset($result[1]);
+    $result = array_values($result);
+
+    return $result;
+}
+
+function setData($dir, $file){
+    $data = file($dir . $file);
+    $result = '';
+    foreach ($data as $value){
+        $result .= $value;
+    }
+    $result = btw($result);
+
+    $guid = json_decode($result);
+    $select = getSelect($result);
+    PWDLS_mysql::getMySQL($select);
+
+    arhive($guid->guid, $dir, $file);
+}
+
+function getSelect($data){
+    $guid = json_decode($data);
+    $select = 'select count(*) as result FROM TV_TEHPAS WHERE tv_guid = "' . $guid->guid . '";';
+    $ob = PWDLS_mysql::getMySQL($select);
+
+    if($ob[0]['result'] == 0){
+        $result = "INSERT INTO TV_TEHPAS (tv_guid, tv_data) VALUES ('" . $guid->guid . "', ' . $data . ');";
+    } else {
+        $result = "UPDATE TV_TEHPAS SET tv_data = ' " . $data . "' WHERE tv_guid = '" . $guid->guid . "';";
+    }
+
+    return $result;
+}
+
+function arhive($name, $lastDir, $fileName){
+    $dir = substr($name, 0, 2);
+    if (!is_dir(ARHIV . $dir)) {
+        mkdir(ARHIV . $dir, 0700);
+    }
+    $dirFile = ARHIV . $dir . '/' . $name . '.json';
+    if (file_exists($dirFile)) unlink($dirFile);
+    rename($lastDir . $fileName, $dirFile);
+}
+
+function btw($b1) {
+    $b1 = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $b1);
+    $b1 = str_replace(array("\r\n", "\r", "\n", "\t"), '', $b1);
+    return $b1;
+}
+
+main();
