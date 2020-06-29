29966b6a2b6f6178de12d53f0285bc1d8b47f8eb
Gribov Igor <work.gribov@gmail.com>
2020-06-30 00:16:29 +0300

    добавил небольшой комментарий в readme

--- getTehPas.php
+++ getTehPas.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=utf-8');
 include_once('class/PWDLS_mysql.class.php');
 $guid = filter_input(INPUT_GET,'guid');
 if(!empty($guid)){
-    $select = 'select tv_data FROM TV_TEHPAS WHERE tv_guid = "' . $guid . '";';
+    $select = 'select tv_data FROM tv_tehpas WHERE tv_guid = "' . $guid . '";';
     $ob = PWDLS_mysql::getMySQL($select);
     if($ob){
         echo $ob[0]['tv_data'];
