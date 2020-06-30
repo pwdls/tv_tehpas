1a3152502a6430f3c3295f10600595ec3486820b
Gribov Igor <work.gribov@gmail.com>
2020-06-30 10:15:24 +0300

    добавил вывод result 1 если guid не найден

--- getTehPas.php
+++ getTehPas.php
@@ -5,7 +5,7 @@ $guid = filter_input(INPUT_GET,'guid');
 if(!empty($guid)){
     $select = 'select tv_data FROM tv_tehpas WHERE tv_guid = "' . $guid . '";';
     $ob = PWDLS_mysql::getMySQL($select);
-    if($ob){
+    if($ob != "FALSE"){
         echo $ob[0]['tv_data'];
     } else {
         echo '{"result":"1"}';
