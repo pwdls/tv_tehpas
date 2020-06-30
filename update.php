9905869aacb978f1ef0859dfc5d973b4c92b3423
Gribov Igor <work.gribov@gmail.com>
2020-06-30 10:32:59 +0300

    добавил валидность обрабатываемого файла

--- update.php
+++ update.php
@@ -4,26 +4,29 @@
 // ini_set('display_startup_errors', 1);
 
 
-define('ARHIV', include_once ("conf/whereArhiv--dir-cfg.php"));
+define('ARHIV', include_once("conf/whereArhiv--dir-cfg.php"));
 include_once('class/PWDLS_mysql.class.php');
 
 
-function main(){
+function main()
+{
     $dir = include_once("conf/whereToGetFales--file-cfg.php");
     $arrDir = getArrDir($dir);
 
-    foreach ($arrDir as $value){
-        setData($dir , $value);
+    foreach ($arrDir as $value) {
+        setData($dir, $value);
     }
 }
 
-function foo($foo){
+function foo($foo)
+{
     echo '<pre>';
     var_dump($foo);
     echo '</pre>';
 }
 
-function getArrDir($dir){
+function getArrDir($dir)
+{
     $result = scandir($dir);
     unset($result[0]);
     unset($result[1]);
@@ -32,27 +35,32 @@ function getArrDir($dir){
     return $result;
 }
 
-function setData($dir, $file){
+function setData($dir, $file)
+{
     $data = file($dir . $file);
     $result = '';
-    foreach ($data as $value){
+    foreach ($data as $value) {
         $result .= $value;
     }
     $result = btw($result);
-
     $guid = json_decode($result);
-    $select = getSelect($result);
-    PWDLS_mysql::getMySQL($select);
 
-    arhive($guid->guid, $dir, $file);
+    if (!empty($guid->guid) && $guid->result == 0) {
+        $select = getSelect($result);
+        PWDLS_mysql::getMySQL($select);
+
+        arhive($guid->guid, $dir, $file);
+    }
+
 }
 
-function getSelect($data){
+function getSelect($data)
+{
     $guid = json_decode($data);
     $select = 'select count(*) as result FROM tv_tehpas WHERE tv_guid = "' . $guid->guid . '";';
     $ob = PWDLS_mysql::getMySQL($select);
 
-    if($ob[0]['result'] == 0){
+    if ($ob[0]['result'] == 0) {
         $result = "INSERT INTO tv_tehpas (tv_guid, tv_data) VALUES ('" . $guid->guid . "', '" . addslashes($data) . "');";
     } else {
         $result = "UPDATE tv_tehpas SET tv_data = '" . addslashes($data) . "' WHERE tv_guid = '" . $guid->guid . "';";
@@ -61,7 +69,8 @@ function getSelect($data){
     return $result;
 }
 
-function arhive($name, $lastDir, $fileName){
+function arhive($name, $lastDir, $fileName)
+{
     $dir = substr($name, 0, 2);
     if (!is_dir(ARHIV . $dir)) {
         mkdir(ARHIV . $dir, 0700);
@@ -71,7 +80,8 @@ function arhive($name, $lastDir, $fileName){
     rename($lastDir . $fileName, $dirFile);
 }
 
-function btw($b1) {
+function btw($b1)
+{
     $b1 = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $b1);
     $b1 = str_replace(array("\r\n", "\r", "\n", "\t"), '', $b1);
     return $b1;
